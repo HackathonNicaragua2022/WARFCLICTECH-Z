47293809331aa543cc0a65021072677a3af09cd3
Javierruizf <ruizfernandez88@gmail.com>
2022-10-23 05:16:05 -0600

    Actualizacion Medico V.2

--- Medico.php
+++ Medico.php
@@ -7,13 +7,12 @@
 <?php include("conexion.php");?>
 	<div class="paginaentrada">
   		<div  class="form" >
-    		<h1 >Atencion De Pacientes</h1> 
-   
-             <h1> Nombre del Paciente: </h1>
+    		<h1 >Atencion De Pacientes</h1>
+            <h1> Nombre del Paciente: </h1>
         <button class="btn-1"> Inicio de Consulta </button>
-        <caption>  </caption>
         <button class="btn-2">Fin de Consulta</button>
         <?php 
+                date_default_timezone_set('America/Costa_Rica');
                 $horayfecha = time();
                 echo date("d-m-Y (H:i:s)", $horayfecha);           
             ?>

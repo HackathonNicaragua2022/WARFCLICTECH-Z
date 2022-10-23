c795fc8ee691453667ffd65b0fd822edecc5abf0
Javierruizf <ruizfernandez88@gmail.com>
2022-10-23 05:21:07 -0600

    Actualizacion de turnapp V.2

--- turnapp.php
+++ turnapp.php
@@ -1,17 +1,21 @@
 <html>
+	<title>TurnApp - Inicio de Sesión</title>
+<link rel="shortcut icon" href="/favicon.ico" />
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-<?php include("conexion.php");?>
 	<div class="paginaentrada">
   		<div  class="form" >
     		<form action="iniciosesion.php" class="formularioentrada" method="post">
+				<h2>Inicio de Sesión</h2>
       			<input id="txt_usuario" name="txt_usuario" type="text" placeholder="Nombre de Usuario o Numero de INSS"/>
       			<input id="txt_pass" name="txt_pass" type="password" placeholder="Contraseña"/>
-      			<button type="submit">Iniciar sesión</button>
-      			<p class="message"><a href="#">Olvidaste tu contraseña?</a></p>
+				<Button><a href="http:\\localhost\turnapp\paciente.php">Iniciar Sesión</a></button>
+      			<!--<button type="submit">Iniciar sesión</button>-->
+      			<p class="message"><a href="http:\\localhost\turnapp\recuperacion.php">Olvidaste tu contraseña?</a></p>
+				  <p class="message"><a href="http:\\localhost\turnapp\registro.php">Registrarse</a></p>
     		</form>
   		</div>
 	</div>

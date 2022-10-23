9a1d719c805c87502893625f9efcf5786138c9f8
Javierruizf <ruizfernandez88@gmail.com>
2022-10-22 21:41:45 -0600

    Conexion con la base de datos

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+    $conn = new mysqli("localhost","root","","turnapp");
+    if ($conn->connect_errno){
+    	echo "No hay conexión (". $conn->connect_errno . ")", $conn->mysqli_connect_error();
+    }
+    else{
+        echo "<script>alert('La conexión se estableció correctamente');</script>";
+    }
+?>

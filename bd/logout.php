7865192b11cddc0df3097b966f4a1a5a430f1e6f
Wilber Migdonio Guzmán Campos <97040679+WGCampos@users.noreply.github.com>
2022-11-27 11:13:48 -0600

    Delete bd directory

--- bd/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-    unset($_SESSION["s_usuario"]);
-    unset($_SESSION["INSS"]);
-    unset($_SESSION["IDDOCTOR"]);
-session_destroy();
-header("Location:../index.php");
-?>
